c5356a7ac3d9c779fd9c83822d2e3478bcad5c40
Marcelina <marcelina.sitko@interia.pl>
2022-02-08 11:35:38 +0100

    fix

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Constants from 'expo-constants';
 import { FRegistrationForm } from 'components/Forms/FRegistrationForm';
 import storybook from './storybook';
