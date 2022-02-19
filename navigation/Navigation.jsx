ec56e1fcf44e548c408f095580c0d1d9d48fb51b
Marcelina <marcelina.sitko@interia.pl>
2022-02-19 16:32:06 +0100

    fix registration navigation options

--- navigation/Navigation.jsx
+++ navigation/Navigation.jsx
@@ -15,7 +15,6 @@ export const Navigation = () => {
           component={RegistrationScreen}
           options={{
             headerShown: false,
-            title: stackNavigatorNames.REGISTRATION,
           }}
         />
       </Stack.Navigator>
