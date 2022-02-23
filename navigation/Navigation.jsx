9da4b1a3f9e5b9c5f2f8670b2d7631e422c5c232
Marcelina <marcelina.sitko@interia.pl>
2022-02-23 08:51:54 +0100

    set auth token refactor

--- navigation/Navigation.jsx
+++ navigation/Navigation.jsx
@@ -1,24 +1,32 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import stackNavigatorNames from 'constants/stackNavigatorNames';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HomepageScreen } from 'screens/Homepage.screen';
 import { LoginScreen } from 'screens/Login.screen';
 import { RegistrationScreen } from 'screens/Registration.screen';
 import * as SecureStore from 'expo-secure-store';
+import { useSelector, useDispatch } from 'react-redux';
+import { setToken } from 'store/auth/authSlice';
 
 export const Navigation = () => {
   const Stack = createNativeStackNavigator();
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuth);
 
-  const getAuthToken = async () => {
-    const token = await SecureStore.getItemAsync('Authorization');
-    return token;
+  useEffect(() => {
+    setAuthToken();
+  }, []);
+
+  const setAuthToken = async () => {
+    const authToken = await SecureStore.getItemAsync('Authorization');
+    dispatch(setToken(authToken));
   };
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {!getAuthToken() ? (
+        {!isAuth ? (
           <>
             <Stack.Screen
               name={stackNavigatorNames.REGISTRATION}
