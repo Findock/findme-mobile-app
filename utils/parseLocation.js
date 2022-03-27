3b1d1850fdb00e2236f83f63dd33a7953516cb9e
Marcelina <marcelina.sitko@interia.pl>
2022-03-27 15:13:51 +0200

    Update parseLocation.js

--- utils/parseLocation.js
+++ utils/parseLocation.js
@@ -1,8 +1,6 @@
-import locales from 'constants/locales';
-
 export const parseLocation = (street, city) => {
   if (street && city) return `${street}, ${city}`;
   if (street && !city) return street;
   if (!street && city) return city;
-  return locales.UNKNOWN_LOCALIZATION;
+  return null;
 };
