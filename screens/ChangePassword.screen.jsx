d423c675d0870aae3fe55fdbd5c046dcb6ac89f8
klaudia.paluch <klaudia.diana.paluch@gmail.com>
2022-03-31 21:20:23 +0200

    change password input states, handlers. new primary img lock

--- screens/ChangePassword.screen.jsx
+++ screens/ChangePassword.screen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FDefaultLayout } from 'layouts/FDefault.layout';
 import icons from 'themes/icons';
 import sizes from 'themes/sizes';
@@ -13,61 +13,85 @@ import { FButton } from 'components/Buttons/FButton';
 import { View, StyleSheet, Platform } from 'react-native';
 import { FImage } from 'components/Composition/FImage';
 import images from 'constants/images';
-import { FKeyboardWrapper } from '../components/Utils/FKeyboardWrapper';
+import { FKeyboardWrapper } from 'components/Utils/FKeyboardWrapper';
 
-export const ChangePasswordScreen = () => (
-  <FDefaultLayout
-    hasFlatList={false}
-    withLogo={false}
-  >
-    <FKeyboardWrapper>
-      <>
-        <View style={styles.imageContainer}>
-          <FImage
-            imagePath={images.CHANGE_PASSWORD()}
-            width={sizes.WIDTH_120}
-            height={sizes.HEIGHT_120}
+export const ChangePasswordScreen = () => {
+  const [
+    oldEmail,
+    setOldEmail,
+  ] = useState('');
+  const [
+    newEmail,
+    setNewEmail,
+  ] = useState('');
+  const [
+    confirmNewEmail,
+    setConfirmNewEmail,
+  ] = useState('');
+
+  const oldEmailInputHandler = (email) => {
+    setOldEmail(email);
+  };
+  const confirmNewEmailInputHandler = (email) => {
+    setConfirmNewEmail(email);
+  };
+  const newEmailInputHandler = (email) => {
+    setNewEmail(email);
+  };
+  return (
+    <FDefaultLayout>
+      <FKeyboardWrapper>
+        <>
+          <View style={styles.imageContainer}>
+            <FImage
+              imagePath={images.CHANGE_PASSWORD()}
+              width={sizes.WIDTH_120}
+              height={sizes.HEIGHT_120}
+            />
+          </View>
+          <FInput
+            iconPlacement={placements.LEFT}
+            type={inputTypes.PASSWORD}
+            icon={icons.LOCK_CLOSED_OUTLINE}
+            placeholder={locales.PASS_OLD_PASSWORD}
+            marginBottom={sizes.MARGIN_30}
+            onChangeText={oldEmailInputHandler}
           />
-        </View>
-        <FInput
-          iconPlacement={placements.LEFT}
-          type={inputTypes.PASSWORD}
-          icon={icons.LOCK_CLOSED_OUTLINE}
-          placeholder={locales.PASS_OLD_PASSWORD}
-          marginBottom={sizes.MARGIN_30}
-        />
-        <FInput
-          iconPlacement={placements.LEFT}
-          type={inputTypes.PASSWORD}
-          icon={icons.LOCK_CLOSED_OUTLINE}
-          placeholder={locales.PASS_NEW_PASSWORD}
-        />
-        <FInput
-          iconPlacement={placements.LEFT}
-          type={inputTypes.PASSWORD}
-          icon={icons.LOCK_CLOSED_OUTLINE}
-          placeholder={locales.REPEAT_NEW_PASSWORD}
-        />
-        <View style={styles.buttonContainer}>
-          <FButton
-            title={locales.CHANGE_PASSWORD}
-            type={buttonTypes.TEXT_BUTTON}
-            backgroundColor={colors.DARK_PRIMARY}
-            color={colors.WHITE}
-            titleWeight={fonts.HEADING_WEIGHT_BOLD}
-            titleSize={fonts.HEADING_MEDIUM}
+          <FInput
+            iconPlacement={placements.LEFT}
+            type={inputTypes.PASSWORD}
+            icon={icons.LOCK_CLOSED_OUTLINE}
+            placeholder={locales.PASS_NEW_PASSWORD}
+            onChangeText={newEmailInputHandler}
           />
-        </View>
-      </>
-    </FKeyboardWrapper>
-  </FDefaultLayout>
-);
+          <FInput
+            iconPlacement={placements.LEFT}
+            type={inputTypes.PASSWORD}
+            icon={icons.LOCK_CLOSED_OUTLINE}
+            placeholder={locales.REPEAT_NEW_PASSWORD}
+            onChangeText={confirmNewEmailInputHandler}
+          />
+          <View style={styles.buttonContainer}>
+            <FButton
+              title={locales.CHANGE_PASSWORD}
+              type={buttonTypes.TEXT_BUTTON}
+              backgroundColor={colors.PRIMARY}
+              color={colors.WHITE}
+              titleWeight={fonts.HEADING_WEIGHT_BOLD}
+              titleSize={fonts.HEADING_MEDIUM}
+            />
+          </View>
+        </>
+      </FKeyboardWrapper>
+    </FDefaultLayout>
+  );
+};
 
 const styles = StyleSheet.create({
   imageContainer: {
     width: sizes.WIDTH_FULL,
     alignItems: placements.CENTER,
-    marginVertical: Platform.OS === 'ios' ? sizes.MARGIN_30 : 0,
+    marginVertical: Platform.OS === 'ios' ? sizes.MARGIN_50 : 0,
   },
   buttonContainer: {
     alignItems: placements.CENTER,
