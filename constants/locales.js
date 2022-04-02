bbe3f47aa66811c3f3f978c6a32809d640424373
klaudia.paluch <klaudia.diana.paluch@gmail.com>
2022-04-02 19:47:21 +0200

    basic TextSwitch with state props, 'react-native-switch-selector' package, locales++, basic story

--- constants/locales.js
+++ constants/locales.js
@@ -72,4 +72,6 @@ export default {
   ONLINE: 'online',
   MESSAGE_SEND_SUCCESS: 'Wiadomość wysłana pomyślnie!',
   PASSWORD_CHANGED_SUCCESSFULLY: 'Hasło zostało zmienione pomyślnie!',
+  LOST: 'Zagubione',
+  FOUND: 'Znalezione',
 };
