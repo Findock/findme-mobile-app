8e4a7c3d5c12ed0da181357c4d08535d0d0993c5
klaudia.paluch <klaudia.diana.paluch@gmail.com>
2022-04-07 23:02:00 +0200

    FHeading remove padding, yarn remove pack

--- components/Inputs/FBigSwitch.jsx
+++ components/Inputs/FBigSwitch.jsx
@@ -4,7 +4,6 @@ import {
 } from 'react-native';
 import colors from 'themes/colors';
 import fonts from 'themes/fonts';
-import placements from 'themes/placements';
 import sizes from 'themes/sizes';
 import { FHeading } from 'components/Composition/FHeading';
 
@@ -15,7 +14,6 @@ export const FBigSwitch = ({
     if (values[0] === value) setValue(values[1]);
     else setValue(values[0]);
   };
-
   return (
     <View style={{
       flexDirection: 'row',
@@ -28,28 +26,27 @@ export const FBigSwitch = ({
         style={[styles.container, value === values[0] ? styles.backgroundChecked : '']}
         onPress={onToggleHandler}
       >
-        <FHeading
-          align={placements.CENTER}
-          weight={fonts.HEADING_WEIGHT_SEMIBOLD}
-          size={fonts.HEADING_MEDIUM}
-          title={labels[0]}
-          color={value === values[0] ? colors.WHITE : colors.BLACK}
-          paddingVertical={sizes.PADDING_10}
-        />
+        <View>
+          <FHeading
+            weight={fonts.HEADING_WEIGHT_SEMIBOLD}
+            size={fonts.HEADING_MEDIUM}
+            title={labels[0]}
+            color={value === values[0] ? colors.WHITE : colors.BLACK}
+          />
+        </View>
       </TouchableOpacity>
-
       <TouchableOpacity
         style={[styles.container, value === values[1] ? styles.backgroundChecked : '']}
         onPress={onToggleHandler}
       >
-        <FHeading
-          align={placements.CENTER}
-          weight={fonts.HEADING_WEIGHT_SEMIBOLD}
-          size={fonts.HEADING_MEDIUM}
-          title={labels[0]}
-          color={value === values[1] ? colors.WHITE : colors.BLACK}
-          paddingVertical={sizes.PADDING_10}
-        />
+        <View>
+          <FHeading
+            weight={fonts.HEADING_WEIGHT_SEMIBOLD}
+            size={fonts.HEADING_MEDIUM}
+            title={labels[0]}
+            color={value === values[1] ? colors.WHITE : colors.BLACK}
+          />
+        </View>
       </TouchableOpacity>
     </View>
   );
