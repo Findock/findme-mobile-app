3e156a9fb23ca177b9707c43121cb30ed2b52127
heavycavalry <klaudia.diana.paluch@gmail.com>
2022-05-04 17:54:47 +0200

    FM-246: isMe prop fix

--- screens/MyAnnouncementsScreen.jsx
+++ screens/MyAnnouncementsScreen.jsx
@@ -2,20 +2,16 @@ import { Dimensions, View } from 'react-native';
 import React from 'react';
 import sizes from 'themes/sizes';
 import colors from 'themes/colors';
-import { useSelector } from 'react-redux';
 import { AnnouncementsList } from './AnnouncementsList';
 
-export const MyAnnouncementsScreen = () => {
-  const me = useSelector((state) => state.me.me);
-  return (
-    <View style={{
-      height: Dimensions.get('screen').height,
-      backgroundColor: colors.WHITE,
-      paddingVertical: sizes.PADDING_30,
-      paddingHorizontal: sizes.PADDING_5,
-    }}
-    >
-      <AnnouncementsList isMe={me} />
-    </View>
-  );
-};
+export const MyAnnouncementsScreen = () => (
+  <View style={{
+    height: Dimensions.get('screen').height,
+    backgroundColor: colors.WHITE,
+    paddingVertical: sizes.PADDING_30,
+    paddingHorizontal: sizes.PADDING_5,
+  }}
+  >
+    <AnnouncementsList isMe="true" />
+  </View>
+);
--- screens/MyFollowedAnnouncements.screen.jsx
+++ screens/MyFollowedAnnouncements.screen.jsx
@@ -1,23 +1,19 @@
 import { Dimensions, View } from 'react-native';
 import colors from 'themes/colors';
 import sizes from 'themes/sizes';
-import { useSelector } from 'react-redux';
 import { AnnouncementsList } from './AnnouncementsList';
 
-export const MyFollowedAnnouncementsScreen = () => {
-  const me = useSelector((state) => state.me.me);
-  return (
-    <View style={{
-      height: Dimensions.get('screen').height,
-      backgroundColor: colors.WHITE,
-      paddingVertical: sizes.PADDING_30,
-      paddingHorizontal: sizes.PADDING_5,
-    }}
-    >
-      <AnnouncementsList
-        isMe={me}
-        onlyFavorites
-      />
-    </View>
-  );
-};
+export const MyFollowedAnnouncementsScreen = () => (
+  <View style={{
+    height: Dimensions.get('screen').height,
+    backgroundColor: colors.WHITE,
+    paddingVertical: sizes.PADDING_30,
+    paddingHorizontal: sizes.PADDING_5,
+  }}
+  >
+    <AnnouncementsList
+      isMe="true"
+      onlyFavorites
+    />
+  </View>
+);
